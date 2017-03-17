31682ec9c2bcb4c1ea26d37e957444c522205c3d
Li Yi <jayceeyili@gmail.com>
2017-03-16 19:50:33 -0700

    query: send example data back to client

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// const request = require('request');
+// const AppService = require('./AppService');
+const data = require('./../data/sampleData');
 
 // ««««««««« configuration »»»»»»»»»
 var port = process.env.PORT || 3000;
@@ -14,11 +17,18 @@ app.get('/', function(req, res) {
   res.send('Hello World!')
 });
 
-app.get('/query', function(req, res) {
-  
+app.post('/query', function(req, res) {
+  // AppService.fetch(req.body)
+    // .then(function(data) {
+    //   res.json(data);
+    // })
+    // .error(function(error) {
+    //   res.send(error);
+    // })
+  res.json(data);
 });
 
 // ««««««««« start app  »»»»»»»»»
 app.listen(port, function() {
-  console.log('Listening on port ', process.env.PORT);
+  console.log('Listening on port ', port);
 });
